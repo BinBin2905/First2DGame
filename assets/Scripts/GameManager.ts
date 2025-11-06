83d4aaf4a39793b3d928fa1eb2b5716d0ba4c483
phoebin <ghilavo@gmail.com>
2025-11-06 20:45:29 +0700

    Generate Map

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,65 @@
+import { BLOCK_SIZE } from "./PlayerController";
+import {
+  _decorator,
+  CCInteger,
+  Component,
+  instantiate,
+  Node,
+  Prefab,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+enum BlockType {
+  BT_NONE,
+  BT_STONE,
+}
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+  @property({ type: Prefab })
+  public boxPrefab: Prefab | null = null;
+  @property({ type: CCInteger })
+  public roadLength: number = 50;
+  private _road: BlockType[] = [];
+
+  start() {
+    this.generateMap();
+  }
+
+  generateMap() {
+    this.node.removeAllChildren();
+    this._road = [];
+    //start Pos
+    this._road.push(BlockType.BT_STONE);
+
+    for (let i = 1; i < this.roadLength; i++) {
+      if (this._road[i - 1] === BlockType.BT_NONE) {
+        this._road.push(BlockType.BT_STONE);
+      } else {
+        this._road.push(Math.floor(Math.random() * 2));
+      }
+    }
+
+    for (let j = 0; j < this._road.length; j++) {
+      let block: Node | null = this.spawnBlockByType(this._road[j]);
+      if (block) {
+        this.node.addChild(block);
+        block.setPosition(j * BLOCK_SIZE, 0, 0);
+      }
+    }
+  }
+
+  spawnBlockByType(type: BlockType) {
+    if (!this.boxPrefab) return null;
+
+    let block: Node | null = null;
+    switch (type) {
+      case BlockType.BT_STONE:
+        block = instantiate(this.boxPrefab);
+        return block;
+    }
+    return block;
+  }
+
+  //   update(deltaTime: number) {}
+}
